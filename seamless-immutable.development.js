23196c456be9e96a05fbb7dca931ee2aaf61fafb
Ales Rechtorik <ales@f13.cz>
2015-11-09 19:29:31 +0100

    add umd definitions

--- seamless-immutable.development.js
+++ seamless-immutable.development.js
@@ -1,4 +1,20 @@
-(function(){
+// UMD (Universal Module Definition)
+// see https://github.com/umdjs/umd/blob/master/returnExports.js
+(function (root, factory) {
+  /*global define, exports, module */
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(factory);
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like enviroments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.returnExports = factory();
+  }
+}(this, function () {
   "use strict";
 
   function addPropertyTo(target, methodName, value) {
@@ -403,4 +419,5 @@
   } else if (typeof global === "object") {
     global.Immutable = Immutable;
   }
-})();
+
+}));
